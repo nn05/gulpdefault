65602de9e8f14201b2f66d45f27776b5c40687ed
urechko.andy <urechko.andry@gmail.com>
2018-09-18 14:17:00 +0300

    add gulp-file-include to html

--- gulp/html.js
+++ gulp/html.js
@@ -2,13 +2,17 @@ var gulp                = require('gulp');
 var cnf                 = require('../package.json').config;
 var plumber             = require('gulp-plumber');
 var notify              = require("gulp-notify");
+var fileinclude         = require("gulp-file-include");
 
 
 
 gulp.task('html', function () {
   return gulp.src(cnf.src.html + '/*.html')
     .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
-    
+    .pipe(fileinclude({
+      prefix: '@@',
+      basepath: '@file'
+    }))
     .pipe(gulp.dest(cnf.dist.html));
 });
  
